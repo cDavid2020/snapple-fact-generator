246a260a5dc32b57a213e312676adb969d0e6cec
cDavid2020 <116908160+cDavid2020@users.noreply.github.com>
2022-12-31 23:02:15 -0600

    js wip

--- src/generator.js
+++ src/generator.js
@@ -1,1 +1,16 @@
- 
+const jokeContainer = document.getElementById("joke");
+const brn = document.getElementsById("btn");
+const url = "https://v2.jokeapi.dev/joke/Programming";
+
+let getJoke = () => {
+fetch(url)
+.then(data => data.json()))
+.then(item => {
+  jokeContainer.textContent = `${item.joke}`;
+});
+  
+  
+  
+  (item.joke));
+}
+getJoke();
